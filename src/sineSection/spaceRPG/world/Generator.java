3f99a3d535c38943418b63e868a1cace9bc647ac
geekman9097 <powerplanets@aol.com>
2017-05-16 21:49:45 -0400

    Allowing for seeding Generator
    RNJesus officially supported

--- src/sineSection/spaceRPG/world/Generator.java
+++ src/sineSection/spaceRPG/world/Generator.java
@@ -2,11 +2,21 @@ package sineSection.spaceRPG.world;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import sineSection.spaceRPG.world.rooms.*;
 
 public class Generator<T> {
 	private List<Class<? extends T>> validTypes = new ArrayList<>();
+	private Random RNJesus;
+	
+	public Generator(long seed) {
+		RNJesus = new Random(seed);
+	}
+	
+	public Generator() {
+		RNJesus = new Random();
+	}
 	
 	public void addType(Class<? extends T> type) {
 		validTypes.add(type);
@@ -14,7 +24,7 @@ public class Generator<T> {
 	
 	private  Class<? extends T> generateType() {
 		Class<? extends T> result;
-		int index = (int) (Math.random() * validTypes.size());
+		int index = RNJesus.nextInt(validTypes.size());
 		result = validTypes.get(index);
 		return result;
 	}
