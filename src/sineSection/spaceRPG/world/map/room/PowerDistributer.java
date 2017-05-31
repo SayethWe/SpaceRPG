96c3befd10fe23d28e6a2bdf42d470343347e3cb
DMWCincy <digitalmaster9@gmail.com>
2017-05-31 14:33:17 -0400

    Moved room to right place
    Moved room to the right package

--- src/sineSection/spaceRPG/world/map/room/PowerDistributer.java
+++ src/sineSection/spaceRPG/world/map/room/PowerDistributer.java
@@ -1,4 +1,4 @@
-package sineSection.spaceRPG.world.rooms;
+package sineSection.spaceRPG.world.map.room;
 
 public class PowerDistributer extends Room implements PowerPlant {
 	static final String DEFAULT_DESCRIPTION = "A room with a tangle of wires running throughout";
