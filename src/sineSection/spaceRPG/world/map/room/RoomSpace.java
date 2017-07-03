1d8942457f90481d60e6d67af027de88aa773c60
geekman9097 <powerplanets@aol.com>
2017-07-02 22:07:24 -0400

    New room. It'll kill you.

---
+++ src/sineSection/spaceRPG/world/map/room/RoomSpace.java
@@ -0,0 +1,15 @@
+package sineSection.spaceRPG.world.map.room;
+
+/**
+ * It's outer space. It kills you.
+ * @author geekman9097
+ *
+ */
+public class RoomSpace extends Room {
+
+	public RoomSpace(String description) {
+		super(description);
+		// TODO Auto-generated constructor stub
+	}
+
+}
