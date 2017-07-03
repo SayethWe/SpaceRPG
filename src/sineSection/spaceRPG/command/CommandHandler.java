36e188ddf9a23a941307037c81a11a0d2229cfcb
geekman9097 <powerplanets@aol.com>
2017-07-02 20:37:26 -0400

    Help Works now!

--- src/sineSection/spaceRPG/command/CommandHandler.java
+++ src/sineSection/spaceRPG/command/CommandHandler.java
@@ -11,8 +11,20 @@ public class CommandHandler {
 		Command comm = CommandParser.parseCommand(command);
 		CommandString c = comm.getCommand();
 		String[] args = comm.getArgs();
+		SpaceRPG master = SpaceRPG.getMaster();
 
 		switch (c) {
+		case UNKNOWN:
+			SpaceRPG.getMaster().writeToGui(FAILURE_MESSAGE);
+		case HELP:
+			if(args.length == 0) {
+				SpaceRPG.getMaster().writeToGui(CommandStrings.getCommands());
+			} else {
+				for (String helpText : args) {
+					master.writeToGui(helpText + ": " + CommandParser.parseCommand(helpText).getCommand().getDescription());
+				}
+			}
+			break;
 		case USE:
 		case TAKE:
 		case SAVE:
@@ -20,17 +32,9 @@ public class CommandHandler {
 		case LISTEN:
 		case INSPECT:
 		case INFO:
-		case UNKNOWN:
-			SpaceRPG.getMaster().writeToGui(FAILURE_MESSAGE);
-		case HELP_ALT:
-		case HELP:
-			if(args.length == 0) {
-				SpaceRPG.getMaster().writeToGui(CommandStrings.getCommands());
-			} else {
-				
-			}
-			break;
 		case GO:
+			master.writeToGui("Not Implemented");
+			break;
 		case DEBUG_DAMAGE:
 			if(args.length > 0) {
 				try {
--- src/sineSection/spaceRPG/command/CommandString.java
+++ src/sineSection/spaceRPG/command/CommandString.java
@@ -5,13 +5,12 @@ public enum CommandString {
 	LISTEN("Listen", ""),
 	USE("Use", ""), TAKE("Take", ""),
 	INFO("Info", "Refresh all information in the HUD"),
-	HELP("Help", "See A list of Commands, or More Detail About a Command \n Args: None, or Command To Learn About"),
-	HELP_ALT("?", "See A list of Commands, or More Detail About a Command \n Args: None, or Command To Learn About"),
+	HELP("Help", "See A list of Commands, or More Detail About a Command \n Args: None, or Command(s) To Learn About"),
 	GO("move", "Move Between Rooms \n Args: Exit You Want To Go Through"),
 	QUIT("Quit", "Leave the Game Without Saving \n No Args"),
 	SAVE("Save", "Save your progress so far \n No Args"),
 	DEBUG_DAMAGE("debug_dmg", "!DEBUG!\nDamage the player by <Number>\n Args: Number of hit points"),
-	UNKNOWN("", "");
+	UNKNOWN("", "Not a valid Command");
 
 	private final String call;
 	private final String description;
