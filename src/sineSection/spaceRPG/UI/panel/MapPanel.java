af1320857c7280526c52fbf27aef4d4c34ea41ca
geekman9097 <powerplanets@aol.com>
2017-07-17 22:50:58 -0400

    Carty Grapho

---
+++ src/sineSection/spaceRPG/UI/panel/MapPanel.java
@@ -0,0 +1,45 @@
+package sineSection.spaceRPG.UI.panel;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import sineSection.spaceRPG.world.map.room.Room;
+
+/**
+ * 
+ * @author geekman9097
+ * TODO have this be added to the hd by a cartography upgrade to the arctan hud system.
+ */
+public class MapPanel extends AbstractPanel {
+	private static final long serialVersionUID = 1876374043517764378L;
+	
+	private static final Color VISITED_ROOM_COLOR = Color.GREEN;
+	private static final Color UNVISITED_ROOM_COLOR = Color.RED;
+	private static final Color TEXT_COLOR = Color.YELLOW;
+	private static final Color UNMAPPED_COLOR = Color.BLACK;
+
+	public MapPanel() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void addRoom(Room r) {
+		
+	}
+	
+	private void draw(Graphics2D g) {
+		
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void attained() {
+		
+	}
+	
+	public void lost() {
+		
+	}
+
+}
